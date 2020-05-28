ce1ad4d46b3528ce0ba2afa14b08bef47dc75655
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-05-28 18:44:00 +0300

    c

---
+++ lesson3/task2/src/counter.js
@@ -0,0 +1,36 @@
+import { createStore } from 'redux';
+
+export const INCREMENT = 'COUNTER/INCREMENT';
+export const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => {
+    return {
+        type: INCREMENT,
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: DECREMENT,
+    };
+};
+
+
+export const counterReducer = (state = 0, action) => {
+    switch(action.type) {
+        case INCREMENT:
+            return state + 1;
+
+        case DECREMENT:
+            return state - 1;
+        
+        default:
+            return state;
+    }
+};
+
+export const store = createStore(
+    counterReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+        window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
