60d1b4e3e3c5cdc0c78f8a341f079ed0fc4a879b
slobodyanuck16 <slobodyanuck@users.noreply.github.com>
2020-06-01 23:35:45 +0300

    c

--- lesson6/task1/src/store.js
+++ lesson6/task1/src/store.js
@@ -3,9 +3,9 @@ import counterReducer from "./counter.reducer";
 
 const logger = (store) => (next) => (action) => {
     console.group(action.type);
-    console.info(action);
-    const result = next(action);
-    console.log("new state: ", store.getState());
+    console.info("dispatching", action);
+    let result = next(action);
+    console.log("next state", store.getState());
     console.groupEnd();
     return result;
 };
